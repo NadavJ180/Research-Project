5c98a503671d5d2e3a3171061811d876f5af85ef
Your Name <you@example.com>
2026-04-13 14:32:47 +0300

    No major changes

--- Heat_Capacity_Graphs.py
+++ Heat_Capacity_Graphs.py
@@ -2,21 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 # Constants
-h = 6.62607015e-34  # Planck's constant (J*s)
-k = 1.380649e-23    # Boltzmann's constant (J/K)
-v = 1e13            # Frequency of the harmonic oscillator (Hz)
+h = 6.62607015e-34  # Planck's constant [J*s]
+kB = 1.380649e-23    # Boltzmann's constant [J/K]
+w = 1e13            # Frequency of the harmonic oscillator [Hz]
 
 # Temperature range
 T = np.linspace(1, 1000, 100)  # Kelvin
 
-def Cv_HO(T, h, k, v):
-    return (h * v / (k * T))**2 * np.exp(h * v / (k * T)) / (np.exp(h * v / (k * T)) - 1)**2
+def Cv_HO(T, h, kB, w):
+    f = h * w / (kB * T)
+    return f**2 * np.exp(f) / (np.exp(f) - 1)**2
 
-def Cv_Box(T, h, k, v):
+def Cv_Box(T, h, kB, w):
     return 
 
 # HO Heat Capacity
-Cv_HO_val = Cv_HO(T, h, k, v)
+Cv_HO_val = Cv_HO(T, h, k, w)
 
 # Box Heat Capacity
 
